dd1b9fd7f4d4cd2253d06cb61e0941e6a8886828
Matthieu Barthelemy <bonsouere@gmail.com>
2022-06-09 08:29:12 +0800

    use lower case for HostConfig properties

--- Sources/DockerClientSwift/Models/ContainerHostConfig.swift
+++ Sources/DockerClientSwift/Models/ContainerHostConfig.swift
@@ -3,112 +3,112 @@ import Foundation
 public struct ContainerHostConfig: Codable {
     
     /// whether to automatically delete the container when iftexits.
-    public var AutoRemove: Bool = false
+    public var autoRemove: Bool = false
     
     /// A list of volume bindings for this container. Each volume binding is a string in one of these forms:
     ///- `host-src:container-dest[:options]` to bind-mount a host path into the container. Both host-src, and container-dest must be an absolute path.
     /// - `volume-name:container-dest[:options]` to bind-mount a volume managed by a volume driver into the container. container-dest must be an absolute path.
-    public var Binds: [String]? = nil
+    public var binds: [String]? = nil
     
     /// Block IO weight (relative weight).
     /// Allowed values range: [ 0 .. 1000 ]
-    public var BlkioWeight: UInt16 = 0
+    public var blkioWeight: UInt16 = 0
     
     /// Block IO weight (relative device weight)
-    public var BlkioWeightDevice: [BlkioWeight]? = []
+    public var blkioWeightDevice: [BlkioWeight]? = []
     
     /// Limit read rate (bytes per second) from a device
-    public var BlkioDeviceReadBps: BlkioRateLimit? = nil
+    public var blkioDeviceReadBps: BlkioRateLimit? = nil
     
     /// Limit write rate (bytes per second) to a device
-    public var BlkioDeviceWriteBps: BlkioRateLimit? = nil
+    public var blkioDeviceWriteBps: BlkioRateLimit? = nil
     
     /// Limit read rate (IO per second) from a device
-    public var BlkioDeviceReadIOps: BlkioRateLimit? = nil
+    public var blkioDeviceReadIOps: BlkioRateLimit? = nil
     
     /// Limit write rate (IO per second) to a device
-    public var BlkioDeviceWriteIOps: BlkioRateLimit? = nil
+    public var blkioDeviceWriteIOps: BlkioRateLimit? = nil
     
     /// A list of kernel capabilities to add to the container. Conflicts with option 'Capabilities'.
-    public var CapAdd: [String]? = nil
+    public var capAdd: [String]? = nil
     
     /// A list of kernel capabilities to drop from the container. Conflicts with option 'Capabilities'.
-    public var CapDrop: [String]? = nil
+    public var capDrop: [String]? = nil
     
     /// Cgroup to use for the container.
-    public var Cgroup: String = ""
+    public var cgroup: String = ""
     
     /// cgroup namespace mode for the container. Possible values are:
     /// "private": the container runs in its own private cgroup namespace
     /// "host": use the host system's cgroup namespace
     /// If not specified, the daemon default is used, which can either be "private" or "host", depending on daemon version, kernel support and configuration.
-    public var CgroupnsMode: String = ""
+    public var cgroupNsMode: String = ""
     
     /// Path to cgroups under which the container's cgroup is created.
     /// If the path is not absolute, the path is considered to be relative to the cgroups path of the init process.
     /// Cgroups are created if they do not already exist.
-    public var CgroupParent: String = ""
+    public var cgroupParent: String = ""
     
     // Windows only
     // public let ConsoleSize: [UInt8]
     
     /// Path to a file where the container ID is written
-    public var ContainerIDFile: String = ""
+    public var containerIdFile: String = ""
     
     /// Windows only
     /// The number of usable CPUs (Windows only).
     /// On Windows Server containers, the processor resource controls are mutually exclusive. The order of precedence is `CPUCount` first, then `CPUShares`, and `CPUPercent` last.
-    public var CpuCount: UInt8 = 0
+    public var cpuCount: UInt8 = 0
     
     /// Windows only
-    public var CpuPercent: UInt8 = 0
+    public var cpuPercent: UInt8 = 0
     
     /// The length of a CPU period in microseconds.
-    public var CpuPeriod: UInt64 = 0
+    public var cpuPeriod: UInt64 = 0
     
     /// Microseconds of CPU time that the container can get in a CPU period.
-    public var CpuQuota: UInt64 = 0
+    public var cpuQuota: UInt64 = 0
     
     /// The length of a CPU real-time period, in microseconds.
     /// Set to 0 to allocate no time allocated to real-time tasks.
-    public var CpuRealtimePeriod: UInt64 = 0
+    public var cpuRealtimePeriod: UInt64 = 0
     
     /// The length of a CPU real-time runtime, in microseconds.
     /// Set to 0 to allocate no time allocated to real-time tasks.
-    public var CpuRealtimeRuntime: UInt64 = 0
+    public var cpuRealtimeRuntime: UInt64 = 0
     
     /// CPUs in which to allow execution (e.g., `0-3`, `0,1`).
-    public var CpusetCpus: String = ""
+    public var cpusetCpus: String = ""
     
     /// Memory nodes (MEMs) in which to allow execution (`0-3`, `0,1`). Only effective on NUMA systems.
-    public var CpusetMems: String = ""
+    public var cpusetMems: String = ""
     
     /// Value representing this container's relative CPU weight versus other containers.
-    public var CpuShares: UInt = 0
+    public var cpuShares: UInt = 0
     
-    public var Devices: [DeviceMapping]? = []
+    public var devices: [DeviceMapping]? = []
     
     /// A list of cgroup rules to apply to the container
-    public var DeviceCgroupRules: [String]? = nil
+    public var deviceCgroupRules: [String]? = nil
     
     /// A list of DNS servers for the container to use.
-    public var Dns: [String]? = []
+    public var dns: [String]? = []
     
     /// A list of DNS options.
-    public var DnsOptions: [String]? = []
+    public var dnsOptions: [String]? = []
     
     /// A list of DNS search domains.
-    public var DnsSearch: [String]? = []
+    public var dnsSearch: [String]? = []
     
     /// A list of hostnames/IP mappings to add to the container's /etc/hosts file. Specified in the form `["hostname:IP"]`.
-    public var ExtraHosts: [String]? = []
+    public var extraHosts: [String]? = []
     
     /// A list of additional groups that the container process will run as.
-    public var GroupAdd: [String]? = nil
+    public var groupAdd: [String]? = nil
     
     /// Run an init inside the container that forwards signals and reaps processes.
     /// This field is omitted if empty, and the default (as configured on the daemon) is used.
-    public var Init: Bool? = false
+    public var `init`: Bool? = false
     
     /// IPC sharing mode for the container. Possible values are:
     /// - `none`: own private IPC namespace, with /dev/shm not mounted
@@ -117,115 +117,183 @@ public struct ContainerHostConfig: Codable {
     /// - `container:<name|id>`: join another (shareable) container's IPC namespace
     /// - `host`: use the host system's IPC namespace
     /// If not specified, daemon default is used, which can either be `private` or `shareable`, depending on daemon version and configuration.
-    public var IpcMode: String = ""
+    public var ipcMode: String = ""
     
     // Windows only.
-    public var Isolation: String = ""
+    public var isolation: String = ""
     
     /// Kernel memory limit in bytes.
     /// **Deprecated**: This field is deprecated as the kernel 5.4 deprecated `kmem.limit_in_bytes`.
     @available(*, deprecated)
-    public var KernelMemory: UInt64 = 0
+    public var kernelMemory: UInt64 = 0
     
     /// Hard limit for kernel TCP buffer memory (in bytes).
-    public var KernelMemoryTCP: UInt64 = 0
+    public var kernelMemoryTcp: UInt64 = 0
     
     /// A list of links for the container in the form `container_name:alias`.
-    public var Links: [String]? = nil
+    public var links: [String]? = nil
     
     // TODO: implement
     // let LogConfig
     // default when create cotainer: "LogConfig\":{\"Type\":\"\",\"Config\":{}}
     
     /// The list of paths to be masked inside the container (this overrides the default set of paths).
-    public var MaskedPaths: [String]? = nil
+    public var maskedPaths: [String]? = nil
     
     /// Memory limit in bytes.
-    public var Memory: UInt64 = 0
+    public var memory: UInt64 = 0
     
     /// Memory soft limit in bytes.
-    public var MemoryReservation: UInt64 = 0
+    public var memoryReservation: UInt64 = 0
     
     /// Total memory limit (memory + swap). Set as -1 to enable unlimited swap.
-    public var MemorySwap: UInt64 = 0
+    public var memorySwap: UInt64 = 0
     
     /// Tune a container's memory swappiness behavior. Accepts an integer between 0 and 100.
-    public var MemorySwappiness: Int8? = -1
+    public var memorySwappiness: Int8? = -1
     
     /// Specification for mounts to be added to the container.
-    public var Mounts: [ContainerMount]? = nil
+    public var mounts: [ContainerMount]? = nil
     
     /// CPU quota in units of 10^-9 CPUs.
-    public var NanoCpus: UInt64 = 0
+    public var nanoCpus: UInt64 = 0
     
     /// Network mode to use for this container. Supported standard values are: bridge, host, none, and container:<name|id>. Any other value is taken as a custom network's name to which this container should connect to.
-    public var NetworkMode: String = "default"
+    public var networkMode: String = "default"
     
     /// Disable OOM Killer for the container.
-    public var OomKillDisable: Bool? = false
+    public var oomKillDisable: Bool? = false
     
     /// An integer value containing the score given to the container in order to tune OOM killer preferences.
-    public var OomScoreAdj: Int = 0
+    public var oomScoreAdj: Int = 0
     
     /// Tune a container's PIDs limit. Set `0` or `-1` for unlimited, or `null to not change.
-    public var PidsLimit: UInt64? = 0
+    public var pidsLimit: UInt64? = 0
     
     /// Set the PID (Process) Namespace mode for the container. It can be either:
     /// - `container:<name|id>`: joins another container's PID namespace
     /// - `host`: use the host's PID namespace inside the container
-    public var PidMode: String = ""
+    public var pidMode: String = ""
     
     // let PortBindings
     // default when create: {}
     
     /// Gives the container full access to the host.
-    public var Privileged: Bool = false
+    public var privileged: Bool = false
     
     /// Allocates an ephemeral host port for all of a container's exposed ports.
     /// Ports are de-allocated when the container stops and allocated when the container starts. The allocated port might be changed when restarting the container.
     /// The port is selected from the ephemeral port range that depends on the kernel. For example, on Linux the range is defined by `/proc/sys/net/ipv4/ip_local_port_range`.
-    public var PublishAllPorts: Bool = false
+    public var publishAllPorts: Bool = false
     
     /// The list of paths to be set as read-only inside the container (this overrides the default set of paths).
-    public var ReadonlyPaths: [String]? = nil
+    public var readonlyPaths: [String]? = nil
     
     /// Mount the container's root filesystem as read only.
-    public var ReadonlyRootfs: Bool = false
+    public var readonlyRootfs: Bool = false
     
     // let RestartPolicy
     // default when create: {\"Name\":\"no\",\"MaximumRetryCount\":0}
     
     /// Runtime to use with this container.
-    public var Runtime: String? = nil
+    public var runtime: String? = nil
     
     /// A list of string values to customize labels for MLS systems, such as SELinux.
-    public var SecurityOpt: [String]? = nil
+    public var securityOpt: [String]? = nil
     
     /// Size of `/dev/shm` in bytes. If omitted, the system uses 64MB.
-    public var ShmSize: UInt = 0
+    public var shmSize: UInt = 0
     
     /// Storage driver options for this container, in the form `{"size": "120G"}`.
-    public var StorageOpt: [String:String]? = nil
+    public var storageOpt: [String:String]? = nil
     
-    // let Sysctls
+    /// A list of kernel parameters (sysctls) to set in the container.
+    /// For example: `{"net.ipv4.ip_forward": "1"}`
+    public var sysctls: [String:String]? = nil
     
-    /// A map of container directories which should be replaced by tmpfs mounts, and their corresponding mount options. For example:
-    /// `{ "/run": "rw,noexec,nosuid,size=65536k" }`
-    public var Tmpfs: [String:String]? = nil
+    /// A map of container directories which should be replaced by tmpfs mounts, and their corresponding mount options.
+    /// For example: `{ "/run": "rw,noexec,nosuid,size=65536k" }`
+    public var tmpfs: [String:String]? = nil
     
-    public var Ulimits: [Ulimit]? = nil
+    /// A list of resource limits to set in the container
+    public var ulimits: [Ulimit]? = nil
     
     /// Sets the usernamespace mode for the container when usernamespace remapping option is enabled.
-    public var UsernsMode: String = ""
+    public var userNsMode: String = ""
     
     /// UTS namespace to use for the container.
-    public var UTSMode: String = ""
+    public var utsMode: String = ""
     
     /// Driver that this container uses to mount volumes.
-    public var VolumeDriver: String = ""
+    public var volumeDriver: String = ""
     
     /// A list of volumes to inherit from another container, specified in the form `<container name>[:<ro|rw>]`.
-    public var VolumesFrom: [String]? = nil
+    public var volumesFrom: [String]? = nil
+    
+    enum CodingKeys: String, CodingKey {
+        case autoRemove = "AutoRemove"
+        case binds = "Binds"
+        case blkioWeight = "BlkioWeight"
+        case blkioWeightDevice = "BlkioWeightDevice"
+        case blkioDeviceReadBps = "BlkioDeviceReadBps"
+        case blkioDeviceWriteBps = "BlkioDeviceWriteBps"
+        case blkioDeviceReadIOps = "BlkioDeviceReadIOps"
+        case blkioDeviceWriteIOps = "BlkioDeviceWriteIOps"
+        case capAdd = "CapAdd"
+        case capDrop = "CapDrop"
+        case cgroup = "Cgroup"
+        case cgroupNsMode = "CgroupnsMode"
+        case cgroupParent = "CgroupParent"
+        case containerIdFile = "ContainerIDFile"
+        case cpuCount = "CpuCount"
+        case cpuPercent = "CpuPercent"
+        case cpuPeriod = "CpuPeriod"
+        case cpuQuota = "CpuQuota"
+        case cpuRealtimePeriod = "CpuRealtimePeriod"
+        case cpuRealtimeRuntime = "CpuRealtimeRuntime"
+        case cpusetCpus = "CpusetCpus"
+        case cpuShares = "CpuShares"
+        case devices = "Devices"
+        case deviceCgroupRules = "DeviceCgroupRules"
+        case dns = "Dns"
+        case dnsOptions = "DnsOptions"
+        case dnsSearch = "DnsSearch"
+        case extraHosts = "ExtraHosts"
+        case groupAdd = "GroupAdd"
+        case `init` = "Init"
+        case ipcMode = "IpcMode"
+        case isolation = "Isolation"
+        case kernelMemory = "KernelMemory"
+        case kernelMemoryTcp = "KernelMemoryTCP"
+        case links = "Links"
+        case maskedPaths = "MaskedPaths"
+        case memory = "Memory"
+        case memoryReservation = "MemoryReservation"
+        case memorySwap = "MemorySwap"
+        case memorySwappiness = "MemorySwappiness"
+        case mounts = "Mounts"
+        case nanoCpus = "NanoCpus"
+        case networkMode = "NetworkMode"
+        case oomKillDisable = "OomKillDisable"
+        case oomScoreAdj = "OomScoreAdj"
+        case pidsLimit = "PidsLimit"
+        case pidMode = "PidMode"
+        case privileged = "Privileged"
+        case publishAllPorts = "PublishAllPorts"
+        case readonlyPaths = "ReadonlyPaths"
+        case readonlyRootfs = "ReadonlyRootfs"
+        case runtime = "Runtime"
+        case securityOpt = "SecurityOpt"
+        case shmSize = "ShmSize"
+        case storageOpt = "StorageOpt"
+        case sysctls = "Sysctls"
+        case tmpfs = "Tmpfs"
+        case ulimits = "Ulimits"
+        case userNsMode = "UsernsMode"
+        case utsMode = "UTSMode"
+        case volumeDriver = "VolumeDriver"
+        case volumesFrom = "VolumesFrom"
+    }
     
     public struct BlkioWeight: Codable {
         public var path: String
