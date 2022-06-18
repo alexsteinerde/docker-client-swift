5ca8458f5c17570b83fc6518cd1293e227645c58
Matthieu Barthelemy <bonsouere@gmail.com>
2022-06-18 13:34:39 +0800

    Logs on Linux with tty

--- Tests/DockerClientTests/ServiceTests.swift
+++ Tests/DockerClientTests/ServiceTests.swift
@@ -99,7 +99,7 @@ final class ServiceTests: XCTestCase {
         let spec = ServiceSpec(
             name: name,
             taskTemplate: .init(
-                containerSpec: .init(image: "nginx:latest", tty: true)
+                containerSpec: .init(image: "nginx:latest", tty: false)
             ),
             mode: .init(
                 replicated: .init(replicas: 1)
@@ -111,7 +111,7 @@ final class ServiceTests: XCTestCase {
         // not sure how to improve that, might lead to flaky test
         try await Task.sleep(nanoseconds: 5_000_000_000)
         // TODO: test with tty = false and timestamps = true once bug fixed
-        for try await line in try await client.services.logs(service: service, timestamps: false) {
+        for try await line in try await client.services.logs(service: service, timestamps: true) {
             XCTAssert(line.timestamp != Date.distantPast, "Ensure timestamp is parsed properly")
             //XCTAssert(line.source == .stdout, "Ensure stdout is properly detected")
         }
