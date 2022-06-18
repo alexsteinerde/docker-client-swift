cf8a7ab48ca124d6031bb35e77eedd88938d681b
Matthieu Barthelemy <bonsouere@gmail.com>
2022-06-18 13:26:34 +0800

    Logs on Linux with tty

--- Tests/DockerClientTests/ServiceTests.swift
+++ Tests/DockerClientTests/ServiceTests.swift
@@ -110,7 +110,7 @@ final class ServiceTests: XCTestCase {
         // wait until service is running and Nginx has produced logs
         // not sure how to improve that, might lead to flaky test
         try await Task.sleep(nanoseconds: 5_000_000_000)
-        for try await line in try await client.services.logs(service: service, timestamps: true) {
+        for try await line in try await client.services.logs(service: service, timestamps: false) {
             XCTAssert(line.timestamp != Date.distantPast, "Ensure timestamp is parsed properly")
             //XCTAssert(line.source == .stdout, "Ensure stdout is properly detected")
         }
