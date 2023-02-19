b5b125895b315724cfdfd3373e395b03be45db35
Jack Leow <jleow@tubi.tv>
2023-02-19 12:03:00 -0800

    Xcode reindent code.

--- Tests/DockerClientTests/ContainerTests.swift
+++ Tests/DockerClientTests/ContainerTests.swift
@@ -17,7 +17,7 @@ final class ContainerTests: XCTestCase {
     func testCreateContainers() throws {
         let image = try client.images.pullImage(byName: "hello-world", tag: "latest").wait()
         let container = try client.containers.createContainer(image: image).wait()
-
+        
         XCTAssertEqual(container.command, "/hello")
     }
     
@@ -26,7 +26,7 @@ final class ContainerTests: XCTestCase {
         let _ = try client.containers.createContainer(image: image).wait()
         
         let containers = try client.containers.list(all: true).wait()
-    
+        
         XCTAssert(containers.count >= 1)
     }
     
@@ -57,7 +57,7 @@ final class ContainerTests: XCTestCase {
             
             Hello from Docker!
             This message shows that your installation appears to be working correctly.
-
+            
             To generate this message, Docker took the following steps:
              1. The Docker client contacted the Docker daemon.
              2. The Docker daemon pulled the "hello-world" image from the Docker Hub.
@@ -67,18 +67,17 @@ final class ContainerTests: XCTestCase {
                 executable that produces the output you are currently reading.
              4. The Docker daemon streamed that output to the Docker client, which sent it
                 to your terminal.
-
+            
             To try something more ambitious, you can run an Ubuntu container with:
              $ docker run -it ubuntu bash
-
+            
             Share images, automate workflows, and more with a free Docker ID:
              https://hub.docker.com/
-
+            
             For more examples and ideas, visit:
              https://docs.docker.com/get-started/
-
+            
             """
-
         
         XCTAssertTrue(
             output.hasPrefix(expectedOutputPrefix),
